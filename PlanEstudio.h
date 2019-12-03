45015875834e2e0ec6b7b627380851861bda608b
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-03 04:58:33 -0600

    Update PlanEstudio.h

--- PlanEstudio.h
+++ PlanEstudio.h
@@ -2,6 +2,7 @@
 #ifndef PLAN_ESTUDIO_H
 #define PLAN_ESTUDIO_H
 #include <iostream>
+#include <vector>
 using namespace std;
 struct Materia {
 	char codigo[7];
@@ -10,40 +11,47 @@ struct Materia {
 	int uv;
 	int cantHijos;
 	Materia **hijos;
-	Materia (string _codigo,string _nombre,int uv) {
-		this->codigo[_codigo.size () + 1];
-		_codigo.copy (this->codigo,_codigo.size () + 1);
-		this->codigo[_codigo.size ()] = '\0';
-
-		this->nombre[_nombre.size () + 1];
-		_nombre.copy (this->nombre,_nombre.size () + 1);
-		this->nombre[_nombre.size ()] = '\0';
-
+	Materia (string _codigo,string _nombre,int uv,int cantHijos) {
+		strcpy (codigo,_codigo.c_str ());
+		strcpy (nombre,_nombre.c_str ());
 		this->uv = uv;
-		this->cantHijos = 0;
+		this->cantHijos = cantHijos;
 		hijos = 0;
 	}
-	bool operator==(const Materia &rhs) const {
-		if(codigo != rhs.codigo)
-			return true;
-		return false;
-	}
 	Materia () {}
+};
+
+struct materia {
+	string codigo;
+	string nombre;
+	int uv;
+	int cantHijos;
+	string padre;
+	vector<materia*>hijos;
+	int posicion;
+	materia (string _codigo,string _nombre,int _uv,string _padre) {
+		this->codigo=_codigo.c_str();
+		this->nombre=_nombre.c_str ();
+		this->padre=_padre.c_str ();
+		uv = _uv;
+		cantHijos = 0;
+		posicion = 0;
+	}
 };
 
 
 class PlanEstudio {
 private:
-	Materia *raiz;
+	materia **raiz;
 	bool estaVacio ();
-	void ImprimirRec (Materia*raiz);
-	Materia *buscarRec (Materia *,string);
+	void ImprimirRec (materia **raiz);
+	materia **buscarRec (materia **,string);
+	materia *buscarRec2 (materia *,string);
 public:
-
 	PlanEstudio ();
-	void agregarMateria (string,string,int,string);
-	Materia *buscar (string);
+	void agregar (string codigo,string nombre,int uv,string CodigoPadre);
+	materia **buscar (string);
+	materia *buscar2 (string);
 	void imprimir ();
-
 };
 #endif // !PLAN_ESTUDIO_H
