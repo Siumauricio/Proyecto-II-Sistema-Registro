ee7649eea75a6c9f84ce80fdb2243ebab5af8efc
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-06 06:08:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,109 +12,42 @@
 #include <cstdio>
 using namespace std;
 
-
-void debugPrint ();
-void loadData ();
-
-struct Records {
-	char codigo[7];
-	char nombre[45];
-	char nombreHijos[25];
-	int uv;
-	int cantHijos;
-}Estructura1[15];
-
-
 int main () {
 	Archivo a;
-	//loadData ();
-	//debugPrint ();
-	//a.CrearPlanEstudio ();
-   //a.LeerPlanEstudio ();
-	for(int i = 0; i < 2; i++) {
-		//a.ClasesDisponibles ();
-	}
-	//a.modificarNota ("CCC107",89);
-	int Hijos (0);
-	//cout<<a.LCP104 ("CCC105");
-	a.modificarNota ("FIL101",77);
-	//a.modificarNota()
+	PlanEstudio nuevo;
+	a.Menu ();
+	//a.MatricularClases ("MAT103","GEOMETRIA Y TRIGONOMETRIA",4);
+	
+	/*
+	 MAT102, Nombre: ALGEBRA}
+	 MAT101, Nombre: INTRODUCCION AL ALGEBRA}
+	 Materia { codigo: MAT109, Nombre: CALCULO I}
+	MAT103,
+	 Materia { codigo: CCC104, Nombre: PROGRAMACION I}
+	 Materia { codigo: CCC107, Nombre: INTRODUCCION A LA COMPUTACION}
+	nuevo.agregar ("MAT101","INTRODUCCION AL ALGEBRA",4," ");
+	nuevo.agregar ("MAT102","ALGEBRA",4,"MAT101");
+	nuevo.agregar ("CCC107","INTRODUCCION A LA COMPUTACION",4,"MAT101");
+	nuevo.agregar ("CCC104","PROGRAMACION I",4,"CCC107");
+	nuevo.agregar ("LCP104","LAB. PROGRA. I",4,"CCC107");
+	nuevo.agregar ("CCC105","PROGRAMACION II",4,"CCC104");
+	nuevo.agregar ("LCP105","LAB. PROGRA. II",4,"CCC104");
+	nuevo.agregar ("CCC105","PROGRAMACION II",4,"LCP104");
+	nuevo.agregar ("LCP105","LAB. PROGRA. II",4,"LCP104");
+	nuevo.agregar ("MAT103","TRIGO",4,"MAT102");
+	nuevo.agregar ("MAT210","CALCULO",4,"MAT102");
+	nuevo.agregar ("EIE1","ELECTIVA IDIOMA EXTRANJERO I",4,"");
+	nuevo.agregar ("EIE2","ELECTIVA IDIOMA EXTRANJERO II",4,"EIE1");
+	nuevo.agregar ("EIE3","ELECTIVA IDIOMA EXTRANJERO III",4,"EIE2");
+	nuevo.agregar ("SOC101","SOCIOLOGIA",4,"");
+	nuevo.agregar ("FIL101","FILOSOFIA",4,"SOC101");
+	nuevo.imprimir ();
+	*/
+	//;
+	//cout << endl;
+	a.modificarNota ("MAT103",88);
+	a.CrearPlanEstudio ();
+	cout << "\n\n";
 	a.historialAcademico ();
-	cout << "-------------" << endl;
-		a.ClasesDisponibles ();
-
-
-	//loadData (); 
-	//debugPrint ();
-}
-
-void debugPrint () {
-	for(int i = 0; i < 14; i++) {
-		cout << "Codigo: " << Estructura1[i].codigo << endl;
-		cout << "Nombre:" << Estructura1[i].nombre << endl;
-		cout << "UV:" << Estructura1[i].uv << endl;
-		cout << "Clases: " << Estructura1[i].nombreHijos << endl;
-		cout << "N Hijos: " << Estructura1[i].cantHijos << endl;
-		cout << "-------------------------" << endl;
-	}
-}
-
-void loadData () {
-	Archivo a;
-	int i = 0;
-	ifstream in ("I-01.txt");
-	string cadena;
-	smatch match;
-	smatch match2;
-	smatch match3;
-	smatch match4;
-	while(!in.eof ()) {
-
-		if(!in.is_open ()) {
-			cerr << "File can't be opened! " << endl;
-			return;
-		}
-		getline (in,cadena);
-		istringstream entrada (cadena);
-		if(cadena.find ("Nombre") != std::string::npos) {
-			string line (cadena);
-			regex re ("Nombre=(.*)-");
-			if(regex_search (line,match,re)) {
-				string nombre = match[1];
-				strcpy (Estructura1[i].nombre,nombre.c_str());
-			}
-		}
-		istringstream entrada2 (cadena);
-		if(cadena.find ("Codigo") != std::string::npos) {
-			string line (cadena);
-			regex re ("Codigo=(.*)'");
-			if(regex_search (line,match2,re)) {
-				string codigo = match2[1];
-				strcpy (Estructura1[i].codigo,codigo.c_str ());
-			}
-		}
-		istringstream entrada3 (cadena);
-		if(cadena.find ("UV") != std::string::npos) {
-			string line (cadena);
-			regex re ("UV=(.*),");
-			if(regex_search (line,match3,re)) {
-				string UV = match3[1];
-				Estructura1[i].uv=atoi (UV.c_str ());
-			}
-		}
-		istringstream entrada4 (cadena);
-		if(cadena.find ("Clases") != std::string::npos) {
-			string line (cadena);
-			regex re ("Clases=(.*)");
-			if(regex_search (line,match4,re)) {
-				string hijos = match4[1];
-				strcpy(Estructura1[i].nombreHijos,hijos.c_str());
-				Estructura1[i].cantHijos=a.cantidadHijos (hijos);
-			}
-		}
-		i++;
-	}
-
-
 
 }
