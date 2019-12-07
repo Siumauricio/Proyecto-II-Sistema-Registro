90fc1b4316b9c95a8e20c7dd43ab81fb2d401db2
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-07 03:15:14 -0600

    Update PlanEstudio.h

--- PlanEstudio.h
+++ PlanEstudio.h
@@ -46,19 +46,27 @@ private:
 	materia **buscarRec (materia **,string);
 	materia *buscarRec2 (materia *,string);
 public:
+	vector<materia *>padres;
+	vector<materia *>aprobadas;
+	vector<string>verificadas;
+	vector<materia *>clasesDisponibles;
+	vector<materia *>clases;
+	vector<materia *>aplicarNota;
+	int posicion = 0;
 	PlanEstudio ();
 	void agregar (string codigo,string nombre,int uv,string CodigoPadre);
 	materia **buscar (string);
 	materia *buscar2 (string);
-	vector<materia*>clases;
 	void imprimir ();
 	void ClasesDisponibles ();
 	bool yaAprobo (string);
 	void yaAproboSinReq (materia**);//caso para las clases sin req
 	void yaAproboIdiomas (materia **,int);
 	void yaAproboSubHijos (materia **);
-	void padre (string);
-	vector<materia*> padres (materia *,string);
+	void Padres (string);
+	void extraerPadres (materia *,string);
 	bool verificarAprobadas(int);
+	void MatricularClases ();
+	void subirNota ();
 };
 #endif // !PLAN_ESTUDIO_H
