66fa326c80f4eaecf56be75e493dfe792f977401
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-13 03:01:58 -0600

    Update Estudiante.h

--- Estudiante.h
+++ Estudiante.h
@@ -2,18 +2,20 @@
 #ifndef EMPLEADO_H
 #define EMPLEADO_H
 #include <string>
+#include <vector>
 using namespace std;
 struct EstudianteRegistro {
 	int numeroCuenta;
 	char nombre[30];
-	char codigoPlan[7];
+	char codigoPlan[25];
 	float indiceAcademico;
 	int totClasesAprobadas;
 };
 class Estudiante {
 public:
-	 void ingresarEstudiante ();
-	 void ObtenerEstudiante ();
-
+	void ingresarEstudiante ();
+	void ObtenerEstudiante ();
+	vector<string> ObtenerCarreras ();
+	int clasesAprobadas ();
 };
 #endif // !EMPLEADO_H
