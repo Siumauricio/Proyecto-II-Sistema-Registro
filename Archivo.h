250554bca5faa5d5ab3681b0f4df98dedde66321
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-13 03:01:29 -0600

    Update Archivo.h

--- Archivo.h
+++ Archivo.h
@@ -15,35 +15,17 @@ struct Matricula {
 
 class Archivo {
 public:
-	bool caso1 = true;//Extrae El Padre y lo agrega al arbol
-	bool caso2 = false;//agrega sus hijos a un vector y dicho vector se recorre recursivamente
-	bool case3 = true;//Caso para las clases sin requisito
-	bool case4 = false;//Caso para los idiomas
-	bool case5 = false;
-	vector<MateriaFile>SinRequisitos;
-	vector<string>Idiomas;
-	vector<string>ofi;
-	vector<string>Hijos;
-	vector<string>arr;
 	PlanEstudio plan;
-	MateriaFile Estructura[24];
-	bool arreglar = true;
-	bool hola = false;
-	bool caso6 = false;
 	Archivo ();
 	void MatricularClases (string,string,int);
 	void modificarNota (string,int);
 	string obtenerCarrera ();
 	void historialAcademico ();
-	int cantidadHijos (string);
-	void LeerPlanEstudio (string,int);
 	void CrearPlanEstudio ();
-	int codigoRepetido (string);
-	Matricula obtenerClase (string);
-	void LeerPlan2 (string,int,int);
+	void LeerPlanesEstudio ();
 	void Menu ();
-	void MostrarPlanEstudio ();
+	void MostrarPlanEstudio (string);
+	void cargarArbol ();
 private:
-	void guardarInformacion ();
 };
 #endif // !ARCHIVO_H
