57043db7e13737f8082bcb9a9566efdfb6661d57
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-12-13 03:02:26 -0600

    Update PlanEstudio.h

--- PlanEstudio.h
+++ PlanEstudio.h
@@ -3,17 +3,21 @@
 #define PLAN_ESTUDIO_H
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <list>
+#include <string>
 using namespace std;
 struct MateriaFile {
 	char codigo[7];
 	char nombre[45];
-	char nombreHijos[25]="";
+	//char nombreHijos[30];
 	int uv;
 	int cantHijos;
+	char padre[7];
+	
 	MateriaFile (string _codigo,string _nombre,int uv,int cantHijos) {
 		strcpy (codigo,_codigo.c_str ());
 		strcpy (nombre,_nombre.c_str ());
-		
 		this->uv = uv;
 		this->cantHijos = cantHijos;
 	}
@@ -25,17 +29,18 @@ struct materia {
 	int uv;
 	int cantHijos;
 	string padre;
-	vector<materia*>hijos;
+	vector<materia *>hijos;
 	int posicion;
 	materia (string _codigo,string _nombre,int _uv,string _padre) {
-		this->codigo=_codigo.c_str();
-		this->nombre=_nombre.c_str ();
-		this->padre=_padre.c_str ();
+		this->codigo = _codigo.c_str ();
+		this->nombre = _nombre.c_str ();
+		this->padre = _padre.c_str ();
 		uv = _uv;
 		cantHijos = 0;
 		posicion = 0;
 	}
-
+	materia(){}
+	
 };
 
 class PlanEstudio {
@@ -48,24 +53,20 @@ private:
 public:
 	vector<materia *>padres;
 	vector<materia *>aprobadas;
-	vector<string>verificadas;
-	vector<materia *>clasesDisponibles;
-	vector<materia *>clases;
-	vector<materia *>aplicarNota;
+	vector<materia >clasesDisponibles;
+	vector<materia >aplicarNota;
 	int posicion = 0;
 	PlanEstudio ();
 	void agregar (string codigo,string nombre,int uv,string CodigoPadre);
 	materia **buscar (string);
-	materia *buscar2 (string);
+	materia *BuscarPadres (string);
 	void imprimir ();
 	void ClasesDisponibles ();
 	bool yaAprobo (string);
-	void yaAproboSinReq (materia**);//caso para las clases sin req
-	void yaAproboIdiomas (materia **,int);
 	void yaAproboSubHijos (materia **);
 	void Padres (string);
 	void extraerPadres (materia *,string);
-	bool verificarAprobadas(int);
+	bool verificarAprobadas (int);
 	void MatricularClases ();
 	void subirNota ();
 };
